6e76bc2152c28db03ec1c5c8fd8ef34cc76070af
Udit Choudhary <uditchoudhary@gmail.com>
2022-05-04 23:53:12 +1200

    added cors

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require('cors');
+const authRouter = require("./Routers/AuthRouter");
+const router = require("./Routers/Router");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose.connect(process.env.mongoDbUrl);
+
+let port = process.env.PORT || 8230;
+app.use(cors());
+app.use("/api/auth", authRouter);
+app.use("/api/", router);
+
+
+app.listen(port, (res, err) => {
+    if(err) console.log("Failed to start", err)
+    console.log(`Running on port`, port);
+})
