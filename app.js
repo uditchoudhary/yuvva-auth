b41636a97eee65c52d43c90b10f862451d316ab2
Udit Choudhary <uditchoudhary@gmail.com>
2022-05-05 00:58:06 +1200

    trying fixing cors

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ mongoose.connect(process.env.mongoDbUrl);
 let port = process.env.PORT || 8230;
 
 // app.use(cors({ origin: true, credentials: true }));
-app.options("*", cors());
+// app.options("*", cors());
+app.use(cors());
 app.use("/api/auth", authRouter);
 app.use("/api/", router);
 
