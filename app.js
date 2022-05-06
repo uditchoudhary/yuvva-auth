b0dc6ea589c67b496a63df72a91ed3a42e0b5a11
Udit Choudhary <uditchoudhary@gmail.com>
2022-05-07 05:07:46 +1200

    some test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const authRouter = require("./Routers/AuthRouter");
 const router = require("./Routers/Router");
 const dotenv = require("dotenv");
@@ -11,6 +11,7 @@ dotenv.config();
 mongoose.connect(process.env.mongoDbUrl);
 
 let port = process.env.PORT || 8230;
+console.log("Domain allowed ", process.env.allowDomain);
 app.use(
   cors({
     credentials: true,
@@ -22,8 +23,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/", router);
 
-
 app.listen(port, (res, err) => {
-    if(err) console.log("Failed to start", err)
-    console.log(`Running on port`, port);
-})
+  if (err) console.log("Failed to start", err);
+  console.log(`Running on port`, port);
+});
